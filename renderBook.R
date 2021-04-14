082b8c36c4345241f6ee48eea28c271b6b4ba866
Robert Bischoff <bischrob@gmail.com>
2021-04-13 21:40:50 -0700

    Prepared book for github hosting

--- renderBook.R
+++ renderBook.R
@@ -2,3 +2,6 @@ library(bookdown)
 render_book("",
             bookdown::gitbook(number_sections = F,
                               config = list(download = c('pdf', 'mobi', 'epub'))))
+
+render_book("",pdf_book(number_sections = F))
+render_book("",epub_book(number_sections = F))
